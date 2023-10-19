a46ad19e8260d5967860bfae9448ffda73a8a885
LoreWebDev24 <lorenzo.delre12@gmail.com>
2023-10-19 16:09:57 +0200

    functions optimization

--- js/master.js
+++ js/master.js
@@ -1,31 +1,98 @@
+// MY FUNCTIONS:
+
+function autoCarusel() {
+
+    let currentImageDOMElement = imageDOMElements[indexCurrentImage];
+    currentImageDOMElement.classList.remove('active');
+
+
+    if (indexDirection === 'forward') {
+        indexCurrentImage++;
+
+        // 
+        if (indexCurrentImage > (images.length) - 1) {
+            indexCurrentImage = 0;
+        }
+    } else {
+        indexCurrentImage--;
+
+        if (indexCurrentImage < 0) {
+            indexCurrentImage = images.length - 1
+
+        }
+        // 
+    }
+
+    let nextImageDOMElement = imageDOMElements[indexCurrentImage];
+    nextImageDOMElement.classList.add('active');
+}
+
+function goForward () {
+
+    let currentImageDOMElement = imageDOMElements[indexCurrentImage]
+    currentImageDOMElement.classList.remove('active')
+
+
+    indexCurrentImage++;
+
+    if (indexCurrentImage > (images.length) - 1) {
+        indexCurrentImage = 0
+    }
+
+
+    let nextImageDOMElement = imageDOMElements[indexCurrentImage]
+    nextImageDOMElement.classList.add('active')
+}
+
+
+function goBackward () {
+
+    let currentImageDOMElement = imageDOMElements[indexCurrentImage]
+    currentImageDOMElement.classList.remove('active')
+
+
+    indexCurrentImage--;
+
+    if (indexCurrentImage < 0) {
+        indexCurrentImage = images.length - 1
+
+    }
+
+
+    let prevImageDOMElement = imageDOMElements[indexCurrentImage]
+    prevImageDOMElement.classList.add('active')
+}
+
+
+
 // MY ARRAY OF OBJECTS CONTAINING IMG SRC GAME TITLE AND DESCRIPTION :
 
 const images = [
-	{
-		image: 'img/01.webp',
-		title: "Marvel's Spiderman Miles Morale",
-		text: 'Experience the rise of Miles Morales as the new hero masters incredible, explosive new powers to become his own Spider-Man.',
-	},
-	{
-		image: 'img/02.webp',
-		title: 'Ratchet & Clank: Rift Apart',
-		text: 'Go dimension-hopping with Ratchet and Clank as they take on an evil emperor from another reality.',
-	},
-	{
-		image: 'img/03.webp',
-		title: 'Fortnite',
-		text: 'Grab all of your friends and drop into Epic Games Fortnite, a massive 100 - player face - off that combines looting, crafting, shootouts and chaos.',
-	},
-	{
-		image: 'img/04.webp',
-		title: 'Stray',
-		text: 'Lost, injured and alone, a stray cat must untangle an ancient mystery to escape a long-forgotten city',
-	},
-	{
-		image: 'img/05.webp',
-		title: "Marvel's Avengers",
-		text: "Marvel's Avengers is an epic, third-person, action-adventure game that combines an original, cinematic story with single-player and co-operative gameplay.",
-	},
+    {
+        image: 'img/01.webp',
+        title: "Marvel's Spiderman Miles Morale",
+        text: 'Experience the rise of Miles Morales as the new hero masters incredible, explosive new powers to become his own Spider-Man.',
+    },
+    {
+        image: 'img/02.webp',
+        title: 'Ratchet & Clank: Rift Apart',
+        text: 'Go dimension-hopping with Ratchet and Clank as they take on an evil emperor from another reality.',
+    },
+    {
+        image: 'img/03.webp',
+        title: 'Fortnite',
+        text: 'Grab all of your friends and drop into Epic Games Fortnite, a massive 100 - player face - off that combines looting, crafting, shootouts and chaos.',
+    },
+    {
+        image: 'img/04.webp',
+        title: 'Stray',
+        text: 'Lost, injured and alone, a stray cat must untangle an ancient mystery to escape a long-forgotten city',
+    },
+    {
+        image: 'img/05.webp',
+        title: "Marvel's Avengers",
+        text: "Marvel's Avengers is an epic, third-person, action-adventure game that combines an original, cinematic story with single-player and co-operative gameplay.",
+    },
 ]
 
 
@@ -35,17 +102,19 @@ const images = [
 
 let containerDOMElement = document.querySelector('.container-big')
 
-
-
 let indexCurrentImage = 0;
+let indexDirection = 'forward';
+
 
 let containerPrewDOMElement = document.querySelector('.container-prew')
 
-for (let i = 0; i < images.length; i++ ) {
-    let currentSrc = images[i].image ;
-    let currentTitle = images[i].title ;
-    let currentText = images[i].text ;
-    
+// Genero dall'array tutti i tag con le immagini/titol/descrizione
+
+for (let i = 0; i < images.length; i++) {
+    let currentSrc = images[i].image;
+    let currentTitle = images[i].title;
+    let currentText = images[i].text;
+
 
     let htmlString = `
     <div class="image"> 
@@ -54,7 +123,7 @@ for (let i = 0; i < images.length; i++ ) {
         <p>${currentText}</p>
     </div>
     `
-    
+
     let htmlThumb = `
     <div class="thumbs"> 
         <img src="${currentSrc}">
@@ -66,6 +135,8 @@ for (let i = 0; i < images.length; i++ ) {
 }
 
 
+// Mi recupero tutte le immagini di classe img create
+
 let imageDOMElements = document.querySelectorAll('.image')
 
 
@@ -75,65 +146,28 @@ currentImageDOMElement.classList.add('active')
 
 
 
-document.querySelector(".button-backward").addEventListener("click", function() {
-   
-  
-    let currentImageDOMElement = imageDOMElements[indexCurrentImage]
-    currentImageDOMElement.classList.remove('active')
+document.querySelector(".button-backward").addEventListener("click", function () {
+
+    goBackward ()
+
+});
 
 
-    indexCurrentImage--;
+document.querySelector(".button-forward").addEventListener("click", function () {
 
-    if (indexCurrentImage < 0){
-        indexCurrentImage = images.length-1
-        
-    }
+    goForward ()
 
+});
 
-    let prevImageDOMElement = imageDOMElements[indexCurrentImage]
-    prevImageDOMElement.classList.add('active')
-  });
+document.querySelector('.button-change-dir').addEventListener('click', function () {
 
+    indexDirection = 'backward';
 
-document.querySelector(".button-forward").addEventListener("click", function() {
-   
-  
-    let currentImageDOMElement = imageDOMElements[indexCurrentImage]
-    currentImageDOMElement.classList.remove('active')
-
-
-    indexCurrentImage++;
-    
-    if (indexCurrentImage > (images.length)-1){
-        indexCurrentImage = 0   
-    }
-
-
-    let nextImageDOMElement = imageDOMElements[indexCurrentImage]
-    nextImageDOMElement.classList.add('active')
-
-  });
-
-
-
-
-
+});
 
 const intervalID = setInterval(autoCarusel, 3000);
 
 
- function autoCarusel () {
-    let currentImageDOMElement = imageDOMElements[indexCurrentImage]
-    currentImageDOMElement.classList.remove('active')
 
 
-    indexCurrentImage++;
-    
-    if (indexCurrentImage > (images.length)-1){
-        indexCurrentImage = 0   
-    }
 
-
-    let nextImageDOMElement = imageDOMElements[indexCurrentImage]
-    nextImageDOMElement.classList.add('active')
- }
